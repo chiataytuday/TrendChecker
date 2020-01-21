00cab5cb6e81709967f6d175bd0aaf832e3c28b1
user <user@comui-iMac.local>
2020-01-21 22:35:53 +0900

    Create TrendListDetailViewController2.swift

---
+++ trend/trend/TrendListDetailViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  TrendListDetailViewController2.swift
+//  trend
+//
+//  Created by user on 2020/01/21.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+
+class TrendListDetailViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
