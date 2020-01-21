968c7bea11bdf6c9ec6ced5fcba81e0e17aea555
complexjin <59429747+complexjin@users.noreply.github.com>
2020-01-21 22:03:25 +0900

    Add files via upload

---
+++ TrendChecker-master/trend/trend/All.swift
@@ -0,0 +1,16 @@
+//
+//  All.swift
+//  trend
+//
+//  Created by user on 2020/01/21.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+
+class All : UITableViewCell {
+    
+    @IBOutlet weak var AllLabel: UILabel!
+    
+    
+}
---
+++ TrendChecker-master/trend/trend/Sports.swift
@@ -0,0 +1,15 @@
+//
+//  Sports.swift
+//  trend
+//
+//  Created by user on 2020/01/21.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+
+class Sports : UITableViewCell {
+    
+    @IBOutlet weak var SportsLabel: UILabel!
+    
+}
