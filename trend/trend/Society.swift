097328b395c0a389b14b0db5202124dff260da0a
user <user@comui-iMac.local>
2020-01-21 22:36:02 +0900

    Create Society.swift

---
+++ trend/trend/Society.swift
@@ -0,0 +1,14 @@
+//
+//  Society.swift
+//  trend
+//
+//  Created by user on 2020/01/21.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+
+class Society : UITableViewCell {
+    
+    @IBOutlet weak var SocietyLabel: UILabel!
+}
