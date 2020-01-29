80ef7ff47d613e22ca42a1c82ec901c4a659b55f
user <user@comui-iMac.local>
2020-01-29 10:19:07 +0900

    Update TrendListDetailViewController1.swift

--- trend/trend/TrendListDetailViewController1.swift
+++ trend/trend/TrendListDetailViewController1.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class TrendListDetailViewController1: UIViewController {
+class TrendListDetailViewController1: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     var trendDetail : Trend? = nil
     override func viewDidLoad() {
@@ -17,13 +25,12 @@ class TrendListDetailViewController1: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-<<<<<<< HEAD
+
     override func viewDidAppear(_ animated: Bool) {
         print(self.trendDetail)
     }
 
-=======
->>>>>>> a00e039a2981ebadc977dbd7bb70c0d41764e412
+
     /*
     // MARK: - Navigation
 
