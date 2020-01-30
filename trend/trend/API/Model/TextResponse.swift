357780874bda4eba4becbc877187e798b801c836
GOGADORO <sspkr1792@naver.com>
2020-01-30 14:28:05 +0900

    해당 트랜드의 타임라인 가져오기
    text.swift ,textresponse.swift 추가와 api.swift에 해당 함수 추가

---
+++ trend/trend/API/Model/TextResponse.swift
@@ -0,0 +1,13 @@
+//
+//  TextResponse.swift
+//  trend
+//
+//  Created by apple153 on 30/01/2020.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import Foundation
+
+struct TextResponse : Codable{
+    var statuses: [Text]
+}
