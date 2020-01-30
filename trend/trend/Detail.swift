34269b6d81bf2dad5006643fc5737396a59c1b1c
user <user@comui-iMac.local>
2020-01-30 14:25:55 +0900

    Create Detail.swift

---
+++ trend/trend/Detail.swift
@@ -0,0 +1,16 @@
+//
+//  Detail.swift
+//  trend
+//
+//  Created by user on 2020/01/30.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+class Detail : UITableViewCell {
+    
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    @IBOutlet weak var VolumeLabel2: UILabel!
+    
+}
