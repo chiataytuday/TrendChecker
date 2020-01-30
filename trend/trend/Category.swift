a7a414575c8490102d180f4c3f919605ac710848
user <user@comui-iMac.local>
2020-01-30 17:13:38 +0900

    Create Category.swift

---
+++ trend/trend/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  trend
+//
+//  Created by user on 2020/01/30.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+class Category : UITableViewCell{
+    
+    
+}
