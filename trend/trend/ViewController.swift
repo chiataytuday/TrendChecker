f9daff08f0686eeaa382185a60e85d14927120a4
user <user@comui-iMac.local>
2020-01-31 17:54:50 +0900

    Update ViewController.swift

--- trend/trend/ViewController.swift
+++ trend/trend/ViewController.swift
@@ -8,91 +8,47 @@
 
 import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    @IBOutlet weak var TableViewMain: UITableView!
     
- 
-   /*  func getTrendTwitter() {
-          let task = URLSession.shared.dataTask(with: URL(string: "")!) { (data, response, error) in
-              
-              if let dataJson = data {
-                   
-                
-                  do {
-                      let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
-               
-                     
-                      let name = json["name"] as!Array<Dictionary<String, Any>>
-                      self.trendJson = name
-                      
-                      DispatchQueue.main.async {
-                          self.TableViewMain.reloadData() //
-                      }
-                     
-
-                  }
-                  catch {}
-              }
-             
-          }
-          
-          task.resume()
-      }
-   */
-@IBOutlet weak var TableViewMain: UITableView!
+    var trendlist:[Trend] = []
+    var categories:[String] = ["# 해시태그 이슈","# 정치/사회/경제","# 연예/스포츠","# 그외 이슈"]
+    var contentText:[String] = []
+    var isLoaded:Bool = false
    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.row % 2 == 0 {
-
-            let categorycell = TableViewMain.dequeueReusableCell(withIdentifier: "Category", for: indexPath) as! Category
-            //categorycell.CategoryNameLabel.text = ["# 해시태그 이슈","# 정치/사회/경제","# 연예/스포츠","# 그외 이슈"]
-       if indexPath.row == 0 {
-                categorycell.CategoryNameLabel.text = "# 해시태그 이슈"
-            }
-        if indexPath.row == 2 {
-                           categorycell.CategoryNameLabel.text = "# 정치/사회/경제"
-                       }
-        if indexPath.row == 4 {
-                                            categorycell.CategoryNameLabel.text = "#연예/스포츠"
-                                        }
-        if indexPath.row == 6{
-            categorycell.CategoryNameLabel.text = "# 그외 이슈"
-
-      
-            
-            }
-                                                                      
-                                    
+        if isLoaded {
+            if indexPath.row % 3 == 0 { // 짝수
+                let categorycell = TableViewMain.dequeueReusableCell(withIdentifier: "Category", for: indexPath) as! Category
+                categorycell.CategoryNameLabel.text = self.categories[Int(indexPath.row / 3)]
+                return categorycell
+            } else if indexPath.row % 3 == 1{ // 홀수
+                let allcell = TableViewMain.dequeueReusableCell(withIdentifier: "All", for: indexPath) as! All
                    
-                   
-           
-            
-                       return categorycell
-            
-        } else {
-
-                   let allcell = TableViewMain.dequeueReusableCell(withIdentifier: "All", for: indexPath) as! All
-                  
-            let rowData = trendlist[indexPath.row]
-                   
-          
-               
-                allcell.AllLabel.text = rowData.name
-              
-                allcell.VolumeLabel.text = "\(rowData.tweetVolume)트윗"
+                allcell.AllLabel.text = self.trendlist[Int(indexPath.row / 3)].name
+                allcell.VolumeLabel.text = String(self.trendlist[Int(indexPath.row / 3)].tweetVolume ?? 0)+"트윗"
                           
                 return allcell
-                          
-            
-           
-            
+            }
+            else {
+                let contentcell = TableViewMain.dequeueReusableCell(withIdentifier: "ContentCell", for: indexPath) as! ContentCell
+                contentcell.ContentLabel.text = String(self.contentText[Int(indexPath.row/3)])
+                return contentcell
+            }
+        } else {
+            let categorycell = TableViewMain.dequeueReusableCell(withIdentifier: "Category", for: indexPath) as! Category
+            categorycell.CategoryNameLabel.text = self.categories[indexPath.row]
+            return categorycell
         }
-         
-           
     }
-   var trendlist:[Trend] = []
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.trendlist.count
-      }
+        if isLoaded {
+            return self.trendlist.count + self.categories.count + self.contentText.count
+            
+        } else {
+            return self.categories.count
+        }
+    }
       
    /* func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
@@ -106,7 +62,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        //Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
         
         TableViewMain.delegate = self
         TableViewMain.dataSource = self
@@ -114,13 +70,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         
         MyApi.shared.trend { result in
-            
-        self.trendlist = result
-        self.TableViewMain.reloadData()
+            self.trendlist = result
+            self.isLoaded = true
+            self.TableViewMain.reloadData()
         }
     }
     
-    @IBOutlet weak var CurrentTime: UILabel!
+    /*@IBOutlet weak var CurrentTime: UILabel!
     let timeSelector : Selector = #selector(ViewController.updateTime)
       let interval = 1.0 // 타이머 간격. 1초
       var count = 0 // 타이머가 설정한 간격대로 실행되는지 확인하기 위한 변수
@@ -139,7 +95,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             // 현재날짜(date)를 formatter의 dateFormat에서 설정한 포맷대로 string 메서드를 사용하여 문자열(String)로 변환
          CurrentTime.text = "현재시간 : "+formatter.string(from: date as Date)
         
-    }
+    }*/
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "trendDetail"{
