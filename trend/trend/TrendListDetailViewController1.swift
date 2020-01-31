0e1ec15823350c9dc00cd907efe29ed5f53bec82
user <user@comui-iMac.local>
2020-01-31 14:52:41 +0900

    Update TrendListDetailViewController1.swift

--- trend/trend/TrendListDetailViewController1.swift
+++ trend/trend/TrendListDetailViewController1.swift
@@ -12,7 +12,26 @@ class TrendListDetailViewController1: UIViewController,UITableViewDelegate,UITab
     
     @IBOutlet weak var TableViewDetail: UITableView!
     
-    
+    @IBOutlet weak var CurrentTime2: UILabel!
+    let timeSelector : Selector = #selector(TrendListDetailViewController1.updateTime)
+         let interval = 1.0 // 타이머 간격. 1초
+         var count = 0 // 타이머가 설정한 간격대로 실행되는지 확인하기 위한 변수
+      
+        @objc func updateTime(){
+               // count 값을 문자열로 변환하여 lblCurrentTime.text에 출력
+       //        lblCurrentTime.text = String(count)
+       //        count = count + 1 // count 값을 1 증가
+               let date = NSDate() // 현재 시간을 가져옴
+               
+               
+               let formatter = DateFormatter() // DateFormatter라는 클래스의 상수 formatter를 선언
+               
+               formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
+               // 상수 formatter의 dateFormat 속성을 설정
+               // 현재날짜(date)를 formatter의 dateFormat에서 설정한 포맷대로 string 메서드를 사용하여 문자열(String)로 변환
+            CurrentTime2.text = "현재시간 : "+formatter.string(from: date as Date)
+           
+       }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
      return self.trenddetaillist.count
@@ -27,7 +46,7 @@ class TrendListDetailViewController1: UIViewController,UITableViewDelegate,UITab
                
                     
                     detailcell.TitleLabel.text = rowData.name
-                   
+                    
                    
                                
                      return detailcell
