184dc374c3883925db15dc24a8dfe86dda630805
user <user@comui-iMac.local>
2020-01-31 17:54:59 +0900

    Create ContentCell.swift

---
+++ trend/trend/ContentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContentCell.swift
+//  trend
+//
+//  Created by user on 2020/01/31.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import UIKit
+class ContentCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var ContentLabel: UILabel!
+}
