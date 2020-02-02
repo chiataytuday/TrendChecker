3196548a50091ded268a4acef6c23f9088f957d6
user <user@comui-iMac.local>
2020-02-02 19:35:03 +0900

    Update TrendCategoryViewController.swift

--- trend/trend/TrendCategoryViewController.swift
+++ trend/trend/TrendCategoryViewController.swift
@@ -11,7 +11,7 @@ class TrendCategoryViewController: UIViewController, UITableViewDataSource, UITa
     @IBOutlet weak var TableViewMain: UITableView!
     
     var trendlist:[Trend] = []
-    var categories:[String] = ["# 해시태그 이슈","# 정치/사회/경제","# 연예/스포츠","# 그외 이슈"]
+    var categories:[String] = ["# 해시태그 이슈","# 연예/스포츠","# 정치/사회/경제","# 그외 이슈"]
     var isLoaded:Bool = false
     var timelineData:[[Trend]] = []
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -76,7 +76,7 @@ class TrendCategoryViewController: UIViewController, UITableViewDataSource, UITa
                 var detailController1 = segue.destination as? TrendListViewController
                
                 let indexPathRow = TableViewMain.indexPathForSelectedRow //선택된 테이블뷰셀의 행
-                detailController1?.rows = [(indexPathRow?.row)!] // 다음뷰로 넘길 선택된행의 값
+                detailController1?.selectRow = [(indexPathRow?.row)!] // 다음뷰로 넘길 선택된행의 값
                 if detailController1 != nil {
                     print(trendlist)
                 //detailController1!.trendDetail = trendlist
